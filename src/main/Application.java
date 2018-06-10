a05f4758a3772a41ab944342e69ec4da039eff49
TheChosenHero <thechosenherotch@gmail.com>
2018-06-09 21:45:41 -0500

    created new thread
    new task class prints the numbers 0-999 on a seperate thread to the rest
    of the program

--- src/main/Application.java
+++ src/main/Application.java
@@ -3,7 +3,14 @@ package main;
 public class Application {
 
 	public static void main(String[] args) {
+		
+		Task taskRunner = new Task();
+		taskRunner.start();
+		
+		System.out.println("hello world...");
 
+		Task taskRunner2 = new Task();
+		taskRunner2.start();
 	}
 
 }
